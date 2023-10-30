6f5b40ab4a414a928dd850c48a346ef50f655f00
Vektor <zeltobryxov@mail.ru>
2023-10-30 10:58:58 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const getSqare = (num) => {
+    const result = `${num}` ** 2;
+    return result;
+};
+export { getSqare };
+const isEven = (num) => {
+    if (num % 2 === 0) {
+        return Boolean(true);
+    } else if (num % 2 !== 0) {
+        return Boolean(false);
+    }
+};
+export { isEven };
+const checkType = (text) => {
+    const result = typeof (text);
+    return result;
+};
+export { checkType };
 
